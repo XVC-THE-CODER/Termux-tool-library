ebcc2b7602967ecfa2c90d0c1e56428da89dfd92
XVC-THE-CODER <daffcode01@gmail.com>
2026-08-01 22:43:51 +0700

    Delete command.sh

--- command.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-termux-wake-lock
-termux-setup-storage
-pkg update -y
-pkg upgrade -y
-pkg install -y root-repo
-pkg install -y termux-tools termux-api termux-exec termux-services android-tools tsu -y
-clear
