b107755e97ae4464ddfbba7109d886d8ef991b08
XVC-THE-CODER <daffcode01@gmail.com>
2026-08-01 22:07:34 +0700

    Create command.sh

---
+++ main/command.sh
@@ -0,0 +1,13 @@
+termux-wake-lock
+termux-setup-storage
+pkg update -y
+pkg upgrade -y
+pkg install -y termux-tools
+pkg install -y termux-api
+pkg install -y termux-exec
+pkg install -y termux-services
+pkg install -y android-tools
+pkg install -y root-repo
+pkg install -y tsu
+clear
+termux-wake-lock
