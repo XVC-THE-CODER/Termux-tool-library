27c2274648c9c6f65d23195eed61620c568047ed
XVC-THE-CODER <daffcode01@gmail.com>
2026-08-01 22:41:09 +0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,13 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
 termux-wake-lock
 termux-setup-storage
 pkg update -y
 pkg upgrade -y
-pkg install -y termux-tools
-pkg install -y termux-api
-pkg install -y termux-exec
-pkg install -y termux-services
-pkg install -y android-tools
 pkg install -y root-repo
-pkg install -y tsu
+pkg install -y termux-tools termux-api termux-exec termux-services android-tools tsu -y
 clear
-termux-wake-lock
